ec74bf3c42f05d53f4cd73db387bcd5fcd01e4d2
Arun R G <arunrg.agility@gmail.com>
2024-12-24 09:12:30 +0530

    Select Project Template active items for Project Initiation Display

--- Project/run/jiva/app_organization/mod_project_detail/forms_project_detail.py
+++ Project/run/jiva/app_organization/mod_project_detail/forms_project_detail.py
@@ -2,6 +2,8 @@
 from app_organization.mod_app.all_form_imports import *
 from app_organization.mod_project_detail.models_project_detail import *
 
+from app_organization.mod_project_template.models_project_template import *
+
 class ProjectDetailForm(forms.ModelForm):
     class Meta:
         model = ProjectDetail
@@ -9,6 +11,10 @@ class ProjectDetailForm(forms.ModelForm):
                   ,'start_date', 'end_date']
     def __init__(self, *args, **kwargs):
         super(ProjectDetailForm, self).__init__(*args, **kwargs)
+        
+        # Filter templates where active=True
+        self.fields['template'].queryset = ProjectTemplate.objects.filter(active=True)
+        
         self.helper = FormHelper()  # Note: No need to pass 'self' here
         self.helper.form_show_labels = False
 
